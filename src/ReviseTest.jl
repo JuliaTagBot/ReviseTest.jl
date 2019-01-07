6e28c726948d0806992b24f1ba4c5d90a0335c1d
Rafael Schouten <rafaelschouten@gmail.com>
2019-01-07 22:22:55 +1100

    fix

--- src/ReviseTest.jl
+++ src/ReviseTest.jl
@@ -3,7 +3,7 @@ module ReviseTest
 using Requires
 
 function __init__()
-    @require Blink="ad839575-38b3-5650-b840-f874b8c74a25" begin
+    @require Images="916415d5-f1e6-5110-898d-aaa5f9f070e0" begin
         export testfunc
         include("testfile.jl")
     end
